923c71621d46d02402c7417436e01b13d276a5b3
ruchybrach <brach4a@gmail.com>
2024-09-09 08:56:45 -0400

    complete

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -11,7 +11,7 @@ namespace CPUFramework
 {
     public class SQLUtility
     {
-        public static string ConnectionString = "";
+        public static string ConnectionString = ""; 
 
         public static DataTable GetDataTable(string sqlstatement)
         {
